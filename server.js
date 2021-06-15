d1da770a025c9f3eebefe035c75d888f8ebbbf21
Asha Sanford <sanford.asha97@yahoo.com>
2021-06-14 21:22:14 -0400

    got server running on local host

--- server.js
+++ server.js
@@ -6,5 +6,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(logger("dev"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "notetaker";
+const collections = ["notes"];
+
+const db = mongojs(databaseUrl, collections);
+
+
+  app.listen(3000, () => {
+    console.log("App running on port 3000!");
+  });
 
